72974d427d9510b66304a110bb4f18b7523993c9
Sergei Ivanov <sergey.brd@gmail.com>
2023-10-14 18:17:31 +0400

    Implement calculator main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
+#include "Interpreter.h"
+#include "Lexer.h"
+#include "Parser.h"
+
 #include <iostream>
 
 int main()
 {
-  std::cout << "Hello world!" << std::endl;
+  std::string input;
+  Parser parser;
+  Interpreter interpreter;
+  while (std::getline(std::cin, input))
+  {
+    if (input.empty())
+      break;
+    auto lexemes = Lexer::tokenize(input);
+    auto node = parser.parse(lexemes);
+    auto result = interpreter.eval(node);
+    std::cout << (result.isInteger() ? result.get<int>() : result.get<double>()) << std::endl;
+  }
+
   return 0;
 }
