1d820e2d44039160322dafbf339fde682822f29f
Sergei Ivanov <sergey.brd@gmail.com>
2023-10-14 14:04:24 +0400

    Implement ReturnValue class

---
+++ src/ReturnValue.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cassert>
+
+class ReturnValue
+{
+private:
+  int m_integerValue;
+  double m_floatValue;
+  bool m_isInteger;
+
+public:
+  ReturnValue(int i_value) : m_integerValue(i_value), m_isInteger(true){};
+  ReturnValue(double i_value) : m_floatValue(i_value), m_isInteger(false){};
+
+  bool isInteger() { return m_isInteger; }
+
+  template <typename T>
+  T get() { assert(false); };
+
+  template <>
+  int get()
+  {
+    assert(m_isInteger);
+    return m_integerValue;
+  }
+
+  template <>
+  double get()
+  {
+    if (m_isInteger)
+      return static_cast<double>(m_integerValue);
+    return m_floatValue;
+  }
+};
---
+++ test/ReturnValueTests.cpp
@@ -0,0 +1,18 @@
+#include "ReturnValue.h"
+
+#include <gtest/gtest.h>
+
+TEST(RetrunValueTests, Integer)
+{
+  ReturnValue valueInt(3);
+  EXPECT_TRUE(valueInt.isInteger());
+  EXPECT_EQ(valueInt.get<int>(), 3);
+  EXPECT_NEAR(valueInt.get<double>(), 3, 0.001);
+}
+
+TEST(RetrunValueTests, Float)
+{
+  ReturnValue valueFloat(3.14);
+  EXPECT_FALSE(valueFloat.isInteger());
+  EXPECT_NEAR(valueFloat.get<double>(), 3.14, 0.001);
+}
